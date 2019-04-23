a2ca148da0367fb9db499b2021e727b3d631961b
Muhammad Diar Fathurrohman <muhammaddiarfathurrohman@Diars-Air.local>
2019-04-23 19:29:16 +0700

    seller-login

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app >
+    <v-content class="contentForm">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<style>
+  .contentForm {
+    background-color: white;
+    padding: 10px;
+  }
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
