1314559d19a8d598c74ed40b29cf67d168d6ec9f
Muhammad Diar Fathurrohman <muhammaddiarfathurrohman@Diars-Air.local>
2019-04-25 18:04:49 +0700

    dynamic routing, axios, dan store

---
+++ pages/profile.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout column>
+    <Header />
+    <v-flex xs text-xs-center class="mb-2">
+        <h2>My Profile</h2>
+        <v-card>
+          <v-img
+            src="https://upload.wikimedia.org/wikipedia/commons/3/37/No_person.jpg"
+          ></v-img>
+        </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  import Header from "~/components/Header.vue"
+
+  export default {
+    components: {
+      Header
+    }
+  }
+</script>
